28e784e5e91d38270b90f451793cee5e3926660e
ARAYNAUD <adrien.raynaud@ynov.com>
2022-11-08 16:21:42 +0100

    htmlspecialchars in register

--- register.php
+++ register.php
@@ -11,24 +11,27 @@ $con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if($con === false){
     die("ERREUR : Impossible de se connecter. " . mysqli_connect_error());
 }
-if (!isset($_POST['username'], $_POST['password'], $_POST['email'])) {
+$username = mysqli_real_escape_string($con,htmlspecialchars($_POST['username']));
+ $password = mysqli_real_escape_string($con,htmlspecialchars($_POST['password']));
+ $email = mysqli_real_escape_string($con,htmlspecialchars($_POST['email']));
+if (!isset($username, $password, $email)) {
 	header('Location: registerhtml.php?erreur=1');
 	exit();
 }
-if (empty($_POST['username']) || empty($_POST['password']) || empty($_POST['email'])) {
+if (empty($_POST[$username]) || empty($password) || empty($email)) {
 	header('Location: registerhtml.php?erreur=1');
 	exit();
 }
-if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 	header('Location: registerhtml.php?erreur=2');
 	exit();
 }
-if (preg_match('/[A-Za-z0-9]+/', $_POST['username']) == 0) {
+if (preg_match('/[A-Za-z0-9]+/', $username) == 0) {
   header('Location: registerhtml.php?erreur=3');
   exit();
 }
 if ($stmt = $con->prepare('SELECT id, password FROM users WHERE username = ?')) {
-	$stmt->bind_param('s', $_POST['username']);
+	$stmt->bind_param('s', $email);
 	$stmt->execute();
 	$stmt->store_result();
 	if ($stmt->num_rows > 0) {
@@ -36,9 +39,9 @@ if ($stmt = $con->prepare('SELECT id, password FROM users WHERE username = ?')) 
 		exit();
 	} else {
 if ($stmt = $con->prepare('INSERT INTO users (id,username, password, email) VALUES (?, ?, ?, ?)')) {
-	$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+	$password = password_hash($password, PASSWORD_DEFAULT);
 	$id = IdGenrerate();
-	$stmt->bind_param('ssss',$id, $_POST['username'], $password, $_POST['email']);
+	$stmt->bind_param('ssss',$id, $email, $password, $email);
 	$stmt->execute();
 	echo 'You have successfully registered, you can now login! \n';
 } else {
