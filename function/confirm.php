3c6423762349a93c8fbde4e6d584b597e7ef6551
Nathan BOURRY <nathan.bourry@ynov.com>
2022-11-22 13:29:02 +0100

    merge with NAthan

--- function/confirm.php
+++ function/confirm.php
@@ -4,41 +4,46 @@ $re = new Confirm;
 $re->confirm();
 class Confirm {
     public function __construct() {
-        $this->db = new DBHandler;
-        $this->con = $this->db->connect();
     }
     public function confirm() {
-        if (isset($_GET['username']) && isset($_GET['key']) AND !empty($_GET['username']) AND !empty($_GET['key'])) { // Verify data username and key
-            $username = $this->db->SecurityCheck( $this->con, $_GET['username']);// Set username variable for the request
+        $db = new DBHandler;
+        $con = $db->connect();
+        if (isset($_GET['username']) && isset($_GET['key']) AND !empty($_GET['username']) AND !empty($_GET['key'])) {
+            $username = $db->SecurityCheck($con, $_GET['username']);
             $key = $_GET['key'];
-            $requser =  $this->con->prepare("SELECT * FROM users WHERE name = ? AND confirmkey = ?"); // Select the user with the username and the key
-            $requser-> execute(array($username, $key)); // Execute the request
-            $userexist = $requser->fetch(); // Fetch the result
+            $requser = $con->prepare("SELECT * FROM users WHERE name = ? AND confirmkey = ?");
+            $requser-> execute(array($username, $key));
+            $userexist = $requser->fetch();
+            $userAdd = $db -> getConfimeWithName("users", $username);
             if ($userexist != null) {
                 $user = $requser->fetch();
-                if ($user['confirme'] == 0) { // If the user is not confirmed
-                    $updateUser =  $this->con->prepare("UPDATE users SET confirme = 1 WHERE name = ? AND confirmkey = ?"); // Update the user to confirmed
-                    $updateUser->execute(array($username, $key));
-                    header('Location: ../php_template/loginhtml.php'); // Redirect to the login page
-                    exit();
-                }elseif ($user['confirme'] == 3){ // ICI pq je ne rentre pas !
-                    if(isset($_GET['Host']) AND !empty($_GET['Host']))
-                    {
-                        $Host = $this->db -> getFromDbByParam("users","Name",$_GET['Host']);
-                        $updateUser =  $this->con->prepare("UPDATE users SET GroupID = ".$Host["GroupID"]." WHERE name = ? AND confirmkey = ?");
-                        $updateUser->execute(array($username, $key));
-                        /*$updateUser = $con->prepare("UPDATE users SET confirme = 1 WHERE name = ? AND confirmkey = ?");
-                        $updateUser->execute(array($username, $key));*/
-                        //header('Location: grouphtml.php');
+                if ($user['confirme'] == 0) {
+                    if ($userAdd == 3){
+                        if(isset($_GET['Host']) AND !empty($_GET['Host']))
+                        {
+                            $Host = $db -> getFromDbByParam("users","Name",$_GET['Host']);
+                            $updateUser = $con->prepare("UPDATE users SET GroupID = ? WHERE name = ? AND confirmkey = ?");
+                            $updateUser->execute(array($Host["GroupID"], $username, $key));
+                            $updateUser = $con->prepare("UPDATE users SET confirme = 1 WHERE name = ? AND confirmkey = ?");
+                            $updateUser->execute(array($username, $key));
+                            header('Location: ../php_template/loginhtml.php');
+                            exit();
+                        }
                     }
+                    $updateUser = $con->prepare("UPDATE users SET confirme = 1 WHERE name = ? AND confirmkey = ?");
+                    $updateUser->execute(array($username, $key));
+                    header('Location: ../php_template/loginhtml.php');
+                    exit();
+                
                 } else {
-                    echo "your account is already confirmed";
+                    echo "Votre compte a déjà été confirmé !";
                 }
             }else {
-                echo "the user does not exist";
+                echo "L'utilisateur n'existe pas !";
             }
         }else {
-            echo "Error";
+            echo "Erreur";
         }
             }
         }
+?>
